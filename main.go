bb1169cbdecbaf863c8963fccc4d4ef5f3c48ccf
nocd5 <nocd5rd@gmail.com>
2016-03-31 13:26:19 +0900

    Refactoring
    use if statements with variable declaration

--- main.go
+++ main.go
@@ -8,22 +8,19 @@ import (
 
 func main() {
 	args := os.Args[1:]
+	app := "vim.exe"
 
-	is_gui := false
-	g_pos, err := findOption(args, "-g")
-	if err == nil {
-		is_gui = true
+	if g_pos, err := findOption(args, "-g"); err == nil {
 		args = remove(args, g_pos)
+		app = "gvim.exe"
 	}
 
 	is_pipe := false
-	_, err = os.Stdin.Stat()
-	if err == nil {
+	if _, err := os.Stdin.Stat(); err == nil {
 		is_pipe = true
 	}
 	has_dash := false
-	_, err = findOption(args, "-")
-	if err == nil {
+	if _, err := findOption(args, "-"); err == nil {
 		has_dash = true
 	}
 	if is_pipe && !has_dash {
@@ -32,10 +29,6 @@ func main() {
 		args = append([]string{"-"}, args...)
 	}
 
-	app := "vim.exe"
-	if is_gui {
-		app = "gvim.exe"
-	}
 	cmd := exec.Command(app, args...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
